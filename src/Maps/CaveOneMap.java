c8f61be923a90a0bc574d8fe7850177b0edac0ab
MollyAnn O'Malley <mollyann.omalley@gmail.com>
2024-10-24 15:24:56 -0400

    Otis follows alex in every map except for ForestCave for now

--- src/Maps/CaveOneMap.java
+++ src/Maps/CaveOneMap.java
@@ -1,8 +1,10 @@
 package Maps;
 
 import Level.Map;
+import Level.NPC;
 import Tilesets.CaveTileset;
 import Level.Trigger;
+import NPCs.Otis;
 import Scripts.MapTransitions.ForestCaveTransitionScript;
 
 import java.util.ArrayList;
@@ -12,6 +14,17 @@ public class CaveOneMap extends Map {
     public CaveOneMap() {
         super("cave_one_map.txt", new CaveTileset());
         this.playerStartPosition = getMapTile(2, 0).getLocation().addX(24); // middle of path on left
+    }
+
+    @Override
+    public ArrayList<NPC> loadNPCs() {
+        ArrayList<NPC> npcs = new ArrayList<>();
+
+        // Initialize Otis right next to the player
+        Otis otis = new Otis(1, getMapTile(2, 0).getLocation()); // Change coordinates as needed to place Otis next to the player
+        npcs.add(otis);
+
+        return npcs;
     }
 
     @Override
--- src/Maps/ForestThreeMap.java
+++ src/Maps/ForestThreeMap.java
@@ -3,7 +3,9 @@ package Maps;
 import java.util.ArrayList;
 
 import Level.Map;
+import Level.NPC;
 import Level.Trigger;
+import NPCs.Otis;
 import Scripts.MapTransitions.ForestCaveTransitionScript;
 import Scripts.MapTransitions.ForestTwoTransitionScript;
 import Tilesets.ForestTileset;
@@ -13,6 +15,17 @@ public class ForestThreeMap extends Map {
     public ForestThreeMap() {
         super("forest_three_map.txt", new ForestTileset());
         this.playerStartPosition = getMapTile(24, 3).getLocation().subtractX(24).subtractY(24); // middle of path at top
+    }
+
+    @Override
+    public ArrayList<NPC> loadNPCs() {
+        ArrayList<NPC> npcs = new ArrayList<>();
+
+        // Initialize Otis right next to the player
+        Otis otis = new Otis(1, getMapTile(24, 3).getLocation()); // Change coordinates as needed to place Otis next to the player
+        npcs.add(otis);
+
+        return npcs;
     }
 
     @Override
