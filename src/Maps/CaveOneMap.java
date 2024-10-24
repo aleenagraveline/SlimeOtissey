6e5fd7870a0b1f84ced77382aea33a3d3c94e6a6
jjcac <jcac0217@gmail.com>
2024-10-24 15:08:25 -0400

    the world v2

--- src/Maps/CaveOneMap.java
+++ src/Maps/CaveOneMap.java
@@ -11,7 +11,7 @@ public class CaveOneMap extends Map {
 
     public CaveOneMap() {
         super("cave_one_map.txt", new CaveTileset());
-        this.playerStartPosition = getMapTile(0, 7).getLocation(); // middle of path on left
+        this.playerStartPosition = getMapTile(2, 0).getLocation().addX(24); // middle of path on left
     }
 
     @Override
@@ -19,7 +19,7 @@ public class CaveOneMap extends Map {
         ArrayList<Trigger> triggers = new ArrayList<>();
 
         // ForestCave transition trigger
-        triggers.add(new Trigger(0, 336, 12, 96, new ForestCaveTransitionScript()));
+        triggers.add(new Trigger(96, 0, 96, 12, new ForestCaveTransitionScript()));
         return triggers;
 
     }
